a8d6f485653696efdd5caa77b3ddaa5117c8d63d
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-12-15 17:10:13 +0900

    feat: implement basic functions

---
+++ errctx.go
@@ -0,0 +1,64 @@
+// Package errctx is a library for structured error.
+package errctx
+
+import (
+	"fmt"
+	"strings"
+)
+
+type (
+	// Error is a structured error.
+	Error struct {
+		err    error
+		msgs   []string
+		fields Fields
+	}
+
+	// Fields is structured data of error.
+	Fields map[string]interface{}
+)
+
+func initFields(fields Fields) Fields {
+	if fields == nil {
+		return Fields{}
+	}
+	return fields
+}
+
+// Wrap returns an error added fields and msgs.
+// err should not be nil.
+func Wrap(err error, fields Fields, msgs ...string) Error {
+	if e, ok := err.(Error); ok {
+		e.msgs = append(e.msgs, msgs...)
+		e.fields = initFields(e.fields)
+		for k, v := range fields {
+			e.fields[k] = v
+		}
+		return e
+	}
+	return Error{err: err, msgs: msgs, fields: fields}
+}
+
+// Cause returns a base error.
+func (e Error) Cause() error {
+	return e.err
+}
+
+// Error returns a message represents error.
+func (e Error) Error() string {
+	msg := strings.Join(e.msgs, " : ")
+	if len(e.msgs) == 0 {
+		return e.err.Error()
+	}
+	return fmt.Sprintf("%s : %s", e.err, msg)
+}
+
+// Fields returns structured data of error.
+func (e Error) Fields() Fields {
+	return e.fields
+}
+
+// Msgs returns messages.
+func (e Error) Msgs() []string {
+	return e.msgs
+}
---
+++ errctx_test.go
@@ -0,0 +1,71 @@
+package errctx
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_initFields(t *testing.T) {
+	require.NotNil(t, initFields(nil))
+	fields := Fields{"foo": "bar"}
+	require.Equal(t, fields, initFields(fields))
+}
+
+func TestWrap(t *testing.T) {
+	data := []struct {
+		err       error
+		fields    Fields
+		msgs      []string
+		expFields Fields
+		expMsgs   []string
+	}{{
+		fmt.Errorf("foo"), nil, nil, nil, nil,
+	}, {
+		Error{err: fmt.Errorf("foo")}, Fields{"foo": "bar"}, nil, Fields{"foo": "bar"}, nil,
+	}}
+	for _, d := range data {
+		e := Wrap(d.err, d.fields, d.msgs...)
+		require.NotNil(t, e)
+		require.Equal(t, d.expFields, e.Fields())
+		require.Equal(t, d.expMsgs, e.Msgs())
+	}
+}
+
+func TestErrorCause(t *testing.T) {
+	msg := "foo"
+	err := Error{err: fmt.Errorf(msg)}
+	e := err.Cause()
+	require.NotNil(t, e)
+	require.Equal(t, msg, e.Error())
+}
+
+func TestErrorError(t *testing.T) {
+	err := Wrap(fmt.Errorf("foo"), nil)
+	require.Equal(t, "foo", err.Error())
+	e := Wrap(err, nil, "bar")
+	require.Equal(t, "foo", err.Error())
+	require.Equal(t, "foo : bar", e.Error())
+}
+
+func TestErrorFields(t *testing.T) {
+	data := []struct {
+		fields Fields
+		exp    Fields
+	}{{
+		nil, nil,
+	}, {
+		Fields{"foo": "bar"}, Fields{"foo": "bar"},
+	}}
+	for _, d := range data {
+		err := Error{fields: d.fields}
+		require.Equal(t, d.exp, err.Fields())
+	}
+}
+
+func TestErrorMsgs(t *testing.T) {
+	msgs := []string{"foo", "bar"}
+	err := Error{err: fmt.Errorf("hello"), msgs: msgs}
+	require.Equal(t, msgs, err.Msgs())
+}
